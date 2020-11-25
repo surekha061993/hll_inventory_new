7bf05e940fa0a194ad10030c0123fd905fd1b3ef
Surekha Londhe <67469808+surekha1990@users.noreply.github.com>
2020-11-25 09:50:05 +0530

    My First hll inventory commit

---
+++ hll_inventory_new/HLL_Inventory/src/main/java/com/hllinventory/demo/HllInventoryApplication.java
@@ -0,0 +1,13 @@
+package com.hllinventory.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HllInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HllInventoryApplication.class, args);
+	}
+
+}
